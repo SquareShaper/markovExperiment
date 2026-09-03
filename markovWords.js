b034fabe476ba5befd760dd398e7335df98831b5
SquareShaper <squareshaper@tuta.io>
2026-09-03 22:19:55 +0200

    word rewrite fully working, yay!

--- markovWords.js
+++ markovWords.js
@@ -9,6 +9,7 @@ class MarkovGenerator {
     prompt = [];
     generator = [];
     nextWord = "";
+    delimiters = [".", ",", "!", "?"];
     
     constructor(order) {
         this.order = order;
@@ -24,8 +25,7 @@ class MarkovGenerator {
         for (let i = 0; i < text.length; i++) {
             let word = text[i];
 
-            let delimiters = [".", ",", "!", "?"];
-            for (let delimiter of delimiters) {
+            for (let delimiter of this.delimiters) {
                 if (word.includes(delimiter)) {
                     let words = word.split(delimiter);
                     words = words.map(elem => {if(elem == "") {return delimiter} else {return elem}});
@@ -57,7 +57,7 @@ class MarkovGenerator {
 
     setPrompt(prompt) {
         this.prompt = this.textToList(prompt);
-        this.generator = this.prompt.slice(-3);
+        this.generator = this.prompt.slice(-this.order);
     }
 
     canGetNext() {
@@ -66,28 +66,53 @@ class MarkovGenerator {
 
     getNext() {
         let out = get_random(this.wordTransitions[this.generator]);
-        this.generator = this.generator.slice( -(this.order - 1));
         this.generator.push(out);
+        this.generator = this.generator.slice(-this.order);
         return out;
+    }
+
+    generate(number) {
+        let output = "";
+        for (let i = 0; i < number; i++) {
+            if (this.canGetNext()) {
+                let next = this.getNext();
+                let isDelimiter = false;
+                for (let delimiter of this.delimiters) {
+                    if (next === delimiter) {
+                        isDelimiter = true;
+                    }
+                }
+                if (!isDelimiter) {
+                    next = " " + next;
+                }
+                output += next;
+            } else {
+                break;
+            }
+        }
+
+        return output;
+    }
+
+    getFullConvo(number) {
+        let response = this.generate(number);
+        return this.prompt.join(" ") + response;
+    }
+
+    // don't work yet
+    // Should generate at least 'start' words, then stop when it hits a 
+    // 'delimiter' or has generated 'end' words
+    generateUntilSentenceStopBetween(start, end, delimiter) {
+        
     }
 }
 
-let gen = new MarkovGenerator(1);
+let gen = new MarkovGenerator(2);
 
-let text = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. "
+let text = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. Yellow, black. Yellow, black. Yellow, black. Yellow, black. Ooh, black and yellow! Let's shake it up a little. Barry! Breakfast is ready! Ooming! Hang on a second. Hello? - Barry? - Adam? - Oan you believe this is happening? - I can't. I'll pick you up. Looking sharp. Use the stairs. Your father paid good money for those. Sorry. I'm excited. Here's the graduate. We're very proud of you, son. A perfect report card, all B's. Very proud. Ma! I got a thing going here. - You got lint on your fuzz. - Ow! That's me! - Wave to us! We'll be in row 118,000. - Bye! Barry, I told you, stop flying in the house! - Hey, Adam. - Hey, Barry. - Is that fuzz gel? - A little. Special day, graduation. Never thought I'd make it. Three days grade school, three days high school. Those were awkward. Three days college. I'm glad I took a day and hitchhiked around the hive. You did come back different. - Hi, Barry. - Artie, growing a mustache? Looks good. - Hear about Frankie? - Yeah. - You going to the funeral? - No, I'm not going. Everybody knows, sting someone, you die. Don't waste it on a squirrel. Such a hothead. I guess he could have just gotten out of the way. I love this incorporating an amusement park into our day. That's why we don't need vacations. Boy, quite a bit of pomp... under the circumstances. - Well, Adam, today we are men. - We are! - Bee-men. - Amen! Hallelujah! Students, faculty, distinguished bees, please welcome Dean Buzzwell. Welcome, New Hive Oity graduating class of... ...9:15. That concludes our ceremonies. And begins your career at Honex Industries! Will we pick ourjob today? I heard it's just orientation. Heads up! Here we go. Keep your hands and antennas inside the tram at all times. - Wonder what it'll be like? - A little scary. Welcome to Honex, a division of Honesco and a part of the Hexagon Group. This is it! Wow. Wow. We know that you, as a bee, have worked your whole life to get to the point where you can work for your whole life. Honey begins when our valiant Pollen Jocks bring the nectar to the hive. Our top-secret formula is automatically color-corrected, scent-adjusted and bubble-contoured into this soothing sweet syrup with its distinctive golden glow you know as... Honey! - That girl was hot. - She's my cousin! - She is? - Yes, we're all cousins. - Right. You're right. - At Honex, we constantly strive to improve every aspect of bee existence. These bees are stress-testing a new helmet technology. - What do you think he makes? - Not enough. Here we have our latest advancement, the Krelman. - What does that do? - Oatches that little strand of honey that hangs after you pour it. Saves us millions. Oan anyone work on the Krelman? Of course. Most bee jobs are small ones. But bees know that every small job, if it's done well, means a lot. But choose carefully because you'll stay in the job you pick for the rest of your life. The same job the rest of your life? I didn't know that. What's the difference? You'll be happy to know that bees, as a species, haven't had one day off in 27 million years. So you'll just work us to death? We'll sure try. Wow! That blew my mind! 'What's the difference?'' How can you say that? One job forever? That's an insane choice to have to make. I'm relieved. Now we only have to make one decision in life. But, Adam, how could they never have told us that? Why would you question anything? We're bees. We're the most perfectly functioning society on Earth. You ever think maybe things work a little too well here? Like what? Give me one example. I don't know. But you know what I'm talking about. Please clear the gate. Royal Nectar Force on approach. Wait a second. Oheck it out. - Hey, those are Pollen Jocks! - Wow. I've never seen them this close. They know what it's like outside the hive. Yeah, but some don't come back. - Hey, Jocks! - Hi, Jocks! You guys did great! You're monsters! You're sky freaks! I love it! I love it! - I wonder where they were. - I don't know. Their day's not planned. Outside the hive, flying who knows where, doing who knows what. You can'tjust decide to be a Pollen Jock. You have to be bred for that. Right. Look. That's more pollen than you and I will see in a lifetime. It's just a status symbol. Bees make too much of it. Perhaps. Unless you're wearing it and the ladies see you wearing it. Those ladies? Aren't they our cousins too? Distant. Distant. Look at these two. - Oouple of Hive Harrys. - Let's have fun with them. It must be dangerous being a Pollen Jock. Yeah. Once a bear pinned me against a mushroom! He had a paw on my throat, and with the other, he was slapping me! - Oh, my! - I never thought I'd knock him out. What were you doing during this? Trying to alert the authorities."
 
 gen.setData(text);
 
-console.log(gen.rawInputWords);
+gen.setPrompt("According to");
 
-console.log(gen.wordTransitions);
-
-gen.setPrompt("According to all known laws");
-
-console.log(gen.prompt);
-
-console.log(gen.generator);
-
-console.log(gen.canGetNext());
-
-console.log(gen.wordTransitions[gen.generator]);
+console.log(gen.getFullConvo(120));
