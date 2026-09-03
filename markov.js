50c20ebe42b799c5f6542ba813c592f8b89a4a1d
SquareShaper <squareshaper@tuta.io>
2026-09-03 20:04:17 +0200

    remove old code

--- markov.js
+++ markov.js
@@ -3,41 +3,6 @@ function get_random (list) {
     return list[Math.floor((Math.random()*list.length))];
 }
 
-
-// let markovGen = function(txt) {
-//     this.txt = txt.toLowerCase();
-//     this.order = 3;
-//     this.ngrams = {};
-
-//     this.updateNgrams = function(data) {
-//         for (let i = 0; i <= data.length-this.order; i++) {
-//             let gram = data.substring(i,i+3);
-            
-//             if (!this.ngrams[gram]) {
-//                 this.ngrams[gram] = [];
-//             }
-//             if (data[i+3] != undefined) {
-//                 this.ngrams[gram].push(data[i+3]);
-//             }
-//         }
-//     }
-
-//     this.addPrompt = function(input) {
-//         if (input.length > this.order) {
-//             this.updateNgrams(input);
-//             this.prompt = input.substring(input.length-this.order, input.length);
-//         }
-//     }
-
-//     this.getNext = function() {
-//         let nextLetter = get_random(this.ngrams[this.prompt]);
-//         this.prompt = this.prompt.substring(1,3) + nextLetter;
-//         return nextLetter;
-//     }
-
-//     this.updateNgrams(this.txt);
-// }
-
 // Class rewrite
 class MarkovGenerator {
     ngrams = {};
