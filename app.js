797f96f8465fd74adfea301f1c693dcf02450a0b
SquareShaper <squareshaper@tuta.io>
2026-09-03 20:44:11 +0200

    messing around

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ import { MarkovGenerator } from "./markov.js";
 
 let gen = new MarkovGenerator(3);
 
-gen.setData("The funny dog is a funny fish and a funny cat. ")
+let data = "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible. "
 
-gen.setPrompt("The dog is");
+gen.setData(data);
 
-console.log(gen.getFullConvo(200));
+gen.setPrompt("According");
+
+console.log(gen.getFullConvo(1000));
+
+console.log()
